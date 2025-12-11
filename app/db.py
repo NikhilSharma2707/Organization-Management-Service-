3fc37e28a1e2f5fd8d6eb1eb331e9647ac2a529c
Nikhil Sharma <nikhildevsharma27@gmail.com>
2025-12-11 19:29:58 +0530

    Dynamic

--- app/db.py
+++ app/db.py
@@ -3,4 +3,3 @@ from .config import settings
 
 client = AsyncIOMotorClient(settings.MONGO_URI)
 master_db = client[settings.MASTER_DB]
-# dynamic collections will be created in master_db or other db(s)
